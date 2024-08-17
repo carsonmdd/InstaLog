991c942b0e2bad25df28c47274fdf6b3e20635fd
Carson Davis <cmd52465@gmail.com>
2024-08-17 16:48:56 -0700

    update font size

--- themes/forest-dark.tcl
+++ themes/forest-dark.tcl
@@ -40,7 +40,7 @@ namespace eval ttk::theme::forest-dark {
             -insertwidth 1 \
             -insertcolor $colors(-fg) \
             -fieldbackground $colors(-selectbg) \
-            -font {TkDefaultFont 20} \
+            -font {TkDefaultFont 16} \
             -borderwidth 1 \
             -relief flat
 
